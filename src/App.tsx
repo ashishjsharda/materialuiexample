8595a8e8a81c8a75df27a33c07a6e4a9940ae9b4
Ashish Sharda <ashishjsharda@gmail.com>
2023-01-29 09:43:22 -0600

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import * as React from 'react';
-import {Stack} from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Stack spacing={2} direction="row">
+        <Button variant="contained">Default</Button>
+        <Button variant="contained" color="primary"> Colored</Button>
+   </Stack>
   );
 }
 
